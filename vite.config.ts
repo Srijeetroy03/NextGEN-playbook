c97229e752b6d2e2655b09d8e2f5c9fb42ed66ee
Srijeet Roy <93047739+Srijeetroy03@users.noreply.github.com>
2026-02-09 12:52:43 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: "0.0.0.0",
-      allowedHosts: ["nextgen-playbook.onrender.com"],
+      allowedHosts: ["https://netnexus.onrender.com"],
     },
 
     plugins: [react()],
