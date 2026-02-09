098402d731643161bd053ae213a964232393f7e4
Srijeet Roy <93047739+Srijeetroy03@users.noreply.github.com>
2026-02-09 12:55:35 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: "0.0.0.0",
-      allowedHosts: ["https://netnexus.onrender.com"],
+      allowedHosts: ["netnexus.onrender.com"],
     },
 
     plugins: [react()],
